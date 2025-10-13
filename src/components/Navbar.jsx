f61876cfee4d4a37ed39e76096087c76e8987706
Pranjal <pranjal200525@gmail.com>
2025-10-13 10:12:15 +0530

    fix some bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                     </div>
                 
                 <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
-                    <a className='flex items-center' href="https://github.com/vpsingh16">
+                    <a className='flex items-center' href="https://github.com/pranjal3336">
 
                     <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
                     <span className='font-bold px-2'>GitHub</span>
